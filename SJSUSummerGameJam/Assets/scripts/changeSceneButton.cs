22b42584f6abd4c5219635810ed4d6fb0af273ea
Christian <christianacosta1997@gmail.com>
2024-07-26 17:41:19 -0700

    update
    - update key
    - added pause

--- SJSUSummerGameJam/Assets/scripts/changeSceneButton.cs
+++ SJSUSummerGameJam/Assets/scripts/changeSceneButton.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public GameObject pauseMenuUI;  // Reference to the pause menu UI
+    private bool isPaused = false;  // Flag to check if the game is paused
+
     public void MoveToScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
@@ -13,5 +16,12 @@ public class ChangeScene : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit");
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 }
